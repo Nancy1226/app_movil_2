b181e63f4343966eeb2ffe6b8b2dd715a5d50ba0
Nancy1226 <nancy-12_03@hotmail.com>
2024-09-23 00:24:56 -0600

    [ADD] api integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert'; // Para convertir JSON a Map
+import 'package:dio/dio.dart'; // para solicitudes
+import 'package:flutter/services.dart';
+
 
 void main() => runApp(MiApp());
 
@@ -10,10 +15,143 @@ class MiApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Mi App",
-      home: Inicio(),
+      home: Home(),
     );
   }
 }
+
+class Home extends StatefulWidget{
+ const Home({super.key});
+
+ @override
+ State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<dynamic>? products;
+
+  // Solicitud API
+  Future<void> fetchProducts() async {
+    final dio = Dio();
+
+    try {
+      final response = await dio.get('https://fakestoreapi.com/products');
+      if (response.statusCode == 200) {
+        setState(() {
+          products = response.data;
+        });
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Error: ${response.statusMessage}')));
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Error: $e')));
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchProducts(); // Llama a la API al inicializar
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Productos de Tienda'),
+        backgroundColor: Colors.blueAccent,
+        elevation: 0,
+      ),
+      body: products == null
+          ? Center(child: CircularProgressIndicator())
+          : Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, // Dos columnas
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 0.75,
+                ),
+                itemCount: products!.length,
+                itemBuilder: (context, index) {
+                  final product = products![index];
+                  return Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    elevation: 5,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+                            child: Image.network(
+                              product['image'],
+                              fit: BoxFit.cover,
+                              width: double.infinity,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            product['title'],
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Text(
+                            '\$${product['price']}',
+                            style: TextStyle(color: const Color.fromARGB(255, 0, 105, 150), fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        SizedBox(height: 8), // Espacio al final
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: 0,
+        onDestinationSelected: (int index) {
+          // Navegación según la selección
+        if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Inicio()),
+            );
+          } else if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SegundaVista()),
+            );
+          }
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Carrito',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 
 class Inicio extends StatefulWidget {
   const Inicio({super.key});
@@ -77,8 +215,9 @@ class _InicioState extends State<Inicio> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("AppBar"),
+        title: Text("Segunda vista"),
         backgroundColor: Colors.blueAccent,
+         automaticallyImplyLeading: false,
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.search),
@@ -134,16 +273,22 @@ class _InicioState extends State<Inicio> {
         ),
       ),
       bottomNavigationBar: NavigationBar(
-        selectedIndex: 0,
+        selectedIndex: 1,
         onDestinationSelected: (int index) {
-          if (index == 1) {
+          if (index == 0) {
+            Navigator.pop(context); // Regresa a la vista anterior
+          } else if(index == 2){
             Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => SegundaVista()),
-            );
+                context,
+                MaterialPageRoute(builder: (context) => SegundaVista()),
+              );  
           }
         },
         destinations: const [
+           NavigationDestination(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Carrito',
+          ),
           NavigationDestination(
             icon: Icon(Icons.home),
             label: 'Home',
@@ -159,13 +304,14 @@ class _InicioState extends State<Inicio> {
 }
 
 class SegundaVista extends StatelessWidget {
+
 const SegundaVista({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Segunda Vista"),
+        title: Text("Tercera Vista"),
         backgroundColor: Colors.blueAccent,
         automaticallyImplyLeading: false,
       ),
@@ -210,8 +356,13 @@ const SegundaVista({Key? key}) : super(key: key);
                 padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 20, vertical: 10)),
                 shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))),
               ),
-              onPressed: () {
-                // Acción para el botón Mensaje
+              onPressed: () async {
+              final sms = Uri.parse('sms:9612545998');
+                if (await canLaunchUrl(sms)) {
+                  await launchUrl(sms);
+                } else {
+                  print('Could not launch $sms');
+                }
               },
               child: Text(
                 'Mensaje',
@@ -224,8 +375,8 @@ const SegundaVista({Key? key}) : super(key: key);
                 padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 20, vertical: 10)),
                 shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))),
               ),
-              onPressed: () {
-                final web = Uri.parse('https://codoweb.com/');
+              onPressed: () async {
+                final web = Uri.parse('https://github.com/Nancy1226/app_movil_2.git');
                 if (await canLaunchUrl(web)) {
                   await launchUrl(web, mode: LaunchMode.externalApplication);
                 } else {
@@ -241,13 +392,19 @@ const SegundaVista({Key? key}) : super(key: key);
         ),
       ),
       bottomNavigationBar: NavigationBar(
-        selectedIndex: 1,
+        selectedIndex: 2,
         onDestinationSelected: (int index) {
           if (index == 0) {
             Navigator.pop(context); // Regresa a la vista anterior
+          }else if(index == 1){
+             Navigator.pop(context);
           }
         },
         destinations: const [
+           NavigationDestination(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Carrito',
+          ),
           NavigationDestination(
             icon: Icon(Icons.home),
             label: 'Home',
